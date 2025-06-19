41110075545b775a47906b12749d30f6ea32f945
Nishchey khajuria <nishcheycapture2014@gmail.com>
2025-06-20 01:38:10 +0530

    Fix CORS for production and dev

--- Server/Server.js
+++ Server/Server.js
@@ -9,19 +9,34 @@ const pdfRoutes = require("./Routes/pdfRoutes");
 
 dotenv.config();
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://pdf-doc-5gn6.onrender.com"
+];
+
 app.use(cors({
-  origin: "*"
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed from this origin: " + origin));
+    }
+  },
+  credentials: true
 }));
+app.options("*", cors());
+
 app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 // Routes
 app.use("/auth", authRoutes);
